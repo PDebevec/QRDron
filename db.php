9d044a7b62065cdb8889225766689094f4c2ae59
Peter Debevec <58183800+PDebevec@users.noreply.github.com>
2021-03-28 18:26:25 +0200

    Add files via upload

--- db.php
+++ db.php
@@ -9,6 +9,9 @@ function dronId($povezava, $params){
     return $povezava->query("select * from piloti where dron=" . (int)$params["id"]);
 }
 function prijava($povezava, $ime){
-    return $povezava->query("select * from piloti where ime=" . $ime);
+    return $povezava->query("select * from piloti where ime=" . (string)$ime);
+}
+function dbsql($povezava, $sql){
+    return $povezava->query($sql);
 }
 ?>
